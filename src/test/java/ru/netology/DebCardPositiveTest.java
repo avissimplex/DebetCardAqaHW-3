89baece69571be68a397a36b5290a94c29b15484
Ksenya Sunduova <avissimplex@gmail.com>
2023-06-25 23:47:41 +0400

    Test Passed

---
+++ src/test/java/ru/netology/DebCardPositiveTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class DebCardPositiveTest {
+@Test
+    void ShouldTestPositive() {
+    open ("http://localhost:9999");
+    SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
+    form.$("[name=name]").setValue("Кузьма Петров-Водкин");
+    form.$("[name=phone]").setValue("+77777777777");
+    form.$("[data-test-id=agreement]").click();
+    form.$("[role=button]").click();
+    $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+}
+}
