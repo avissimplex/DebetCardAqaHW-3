337e9fe24e9171f7d01ed69c00fd5319676fa3e6
Ksenya Sunduova <avissimplex@gmail.com>
2023-07-06 09:39:06 +0400

    02/06/23

--- src/test/java/ru/netology/DebCardNegativeTest.java
+++ src/test/java/ru/netology/DebCardNegativeTest.java
@@ -9,31 +9,30 @@ import static com.codeborne.selenide.Selenide.open;
 
 class DebCardNegativeTest {
     @Test
-    void ShouldTestLatinName() {
+    void shouldTestInvalideName() {
         open ("http://localhost:9999");
         SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
         form.$("[name=name]").setValue("Kouzma Petrov-Vodkin");
         form.$("[name=phone]").setValue("+77777777777");
         form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
-        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[class='input__control']").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
 
     }
 
     @Test
-    void ShouldTestOnlyFirstName() {
+    void shouldTestOnlyEmptyName() {
         open ("http://localhost:9999");
         SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
-        form.$("[name=name]").setValue("Кузьма");
         form.$("[name=phone]").setValue("+77777777777");
         form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
-        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[class='input__top']").shouldHave(exactText("Поле обязательно для заполнения"));
 
     }
-
+/*
     @Test
-    void ShouldTestOnlyLastName() {
+    void shouldTestOnlyLastName() {
         open ("http://localhost:9999");
         SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
         form.$("[name=name]").setValue("Иванов");
@@ -45,7 +44,7 @@ class DebCardNegativeTest {
     }
 
     @Test
-    void ShouldTestFalseNumberWithoutPlus() {
+    void shouldTestFalseNumberWithoutPlus() {
         open ("http://localhost:9999");
         SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
         form.$("[name=name]").setValue("Кузьма Петров-Водкин");
@@ -57,7 +56,7 @@ class DebCardNegativeTest {
     }
 
     @Test
-    void ShouldTestFalseNumberWithPlus() {
+    void shouldTestFalseNumberWithPlus() {
         open ("http://localhost:9999");
         SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
         form.$("[name=name]").setValue("Кузьма Петров-Водкин");
@@ -69,7 +68,7 @@ class DebCardNegativeTest {
     }
 
     @Test
-    void ShouldTestNoAgreement() {
+    void shouldTestNoAgreement() {
         open ("http://localhost:9999");
         SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
         form.$("[name=name]").setValue("Кузьма Петров-Водкин");
@@ -77,5 +76,5 @@ class DebCardNegativeTest {
         form.$("[role=button]").click();
         $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
-    }
+    }*/
 }
